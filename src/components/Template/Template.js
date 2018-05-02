1f2fabbe7b2cc98670778bfa38b1056219f9fd0c
Dave Dehghani <you@example.com>
2018-05-01 20:48:25 -0400

    built simple template component directory closes #1

---
+++ src/components/Template/Template.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import {
+  Platform,
+  Text,
+  View
+} from 'react-native'
+
+import styles from './Template.style.js'
+
+const templateInfo = Platform.select({
+  ios: 'welcome to your template component on ios!',
+  android: 'welcome to your template component on android!'
+});
+
+type Props = {};
+export default class Template extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>{templateInfo}</Text>
+      </View>
+    )
+  }
+}
