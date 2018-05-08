426260f370df816e1b5e15b3936cc128c2597878
Dave Dehghani <davedehghani@gmail.com>
2018-05-07 20:32:18 -0400

    updated template file and fixed error for View tag closes #9

--- src/components/Template/Template.style.js
+++ src/components/Template/Template.style.js
@@ -1,9 +1,6 @@
 import { StyleSheet } from 'react-native'
 
 export default styles = StyleSheet.create({
-  container: {
-    position: 'relative',
-    fontSize: 20,
-    textAlign: 'center'
-  },
+  view: {},
+  text: {},
 })
