ec404d37b1e38cd8fef91698eaf5f3c4abd87ac0
Dave Dehghani <davedehghani@gmail.com>
2018-05-23 21:53:15 -0400

    implemented renaming of list functionality closes #40

---
+++ src/components/List/List.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  scrollView: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  listName: {
+    width: '20%'
+  },
+  textInput: {
+    height: 50,
+    width: '15%',
+  },
+})
