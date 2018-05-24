d52800ccf93909ad1ef5d913159dfc95e75b8cf2
Dave Dehghani <davedehghani@gmail.com>
2018-05-23 20:19:06 -0400

    created ability to remove a list based on its uuid closes #37

--- src/state/actions.js
+++ src/state/actions.js
@@ -1,3 +1,5 @@
 import { createAction } from 'redux-actions'
 
 export const createList = createAction('CREATE_LIST', name => ({ name }))
+
+export const deleteList = createAction('DELETE_LIST', listId => ({ listId }))
