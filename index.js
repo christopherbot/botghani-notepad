0e7b33cb38c216ac4b72ded1c64dec54e94f46bd
Dave Dehghani <dave.dehghani@rakuten.com>
2018-11-14 20:28:10 -0500

    added react-native YellowBox api to hide warning caused by react-navigation closes #131

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import { AppRegistry } from 'react-native'
+import { AppRegistry, YellowBox } from 'react-native'
 import BotghaniNotepad from './BotghaniNotepad'
 
+// https://github.com/react-navigation/react-navigation/issues/3956#issuecomment-380648083
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
 
 AppRegistry.registerComponent('botghaniNotepad', () => BotghaniNotepad)
