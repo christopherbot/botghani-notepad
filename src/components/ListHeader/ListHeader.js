1891f81c0cf43b675c7fb77a026acbdf3807b7b2
Dave Dehghani <davedehghani@gmail.com>
2018-11-21 20:49:58 -0500

    septerated the Nav Button from the ListHeader component connected both NavButton + ListHeader component to redux inside of the App's new top navigation bar the headerLeft component is NavButton and the headerTitle component is now ListHeader closes #96

--- src/components/ListHeader/ListHeader.js
+++ src/components/ListHeader/ListHeader.js
@@ -3,26 +3,21 @@ import { connect } from 'react-redux'
 import { View } from 'react-native'
 import PropTypes from 'prop-types'
 
-import NavButton from '../Buttons/NavButton/NavButton'
 import ButtonMenu from '../ButtonMenu/ButtonMenu'
-import { toggleNav } from '../../state/actions'
 
 import ustyle from '../../utils/style'
 
-const ListHeader = ({ list, isNavOpen, toggleNav }) =>
+const ListHeader = ({ list }) =>
   <View style={ustyle.fr}>
     { list && <ButtonMenu list={list}/> }
-    <NavButton onPress={toggleNav} isNavOpen={isNavOpen} />
   </View>
-
 
 ListHeader.propTypes = {
   list: PropTypes.object,
-  isNavOpen : PropTypes.bool,
 }
 
-const mapDispatchToProps = {
-  toggleNav,
-}
+const mapStateToProps = ({ lists, globalUi }) => ({
+  list: lists.find(list => list.id === globalUi.activeListId),
+})
 
-export default connect(null, mapDispatchToProps)(ListHeader)
+export default connect(mapStateToProps)(ListHeader)
