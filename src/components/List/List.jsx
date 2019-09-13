c3026f577f023d2d5d5758d76f9e57b4ef912d03
Dave Dehghani <davedehghani@gmail.com>
2019-09-12 22:01:04 -0400

    deleted List component closes #212

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,19 +1,117 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { PureComponent } from 'react'
+import { View, ScrollView, TextInput } from 'react-native'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { generateFirstColumn, generateColumn } from 'utils/table'
+import { createRow, createColumn } from 'state/actions'
 import { listPropType } from 'components/propTypeDefinitions'
-
-import Table from 'components/Table/Table'
-
+import AddButton from 'components/Buttons/AddButton/AddButton'
 import gStyle from 'styles/globalStyle'
+import { Column, FirstColumn } from './Column'
 import style from './List.style'
+import columnStyle from './Column.style'
 
-const List = ({ list }) =>
-  <View style={[gStyle.fc1, style.list]}>
-    <Table list={list} />
-  </View>
+class List extends PureComponent {
+  static propTypes = {
+    list: listPropType.isRequired,
+    createRow: PropTypes.func.isRequired,
+    createColumn: PropTypes.func.isRequired,
+  }
 
-List.propTypes = {
-  list: listPropType.isRequired,
+  state = {
+    columnName: '',
+    isInputDisplayed: false,
+  }
+
+  get columnName() {
+    return this.state.columnName.trim()
+  }
+
+  createColumn = () => this.props.createColumn(this.props.list.id, this.columnName)
+
+  onChangeText = columnName => this.setState({ columnName })
+
+  onBlur = () => {
+    if (this.columnName) {
+      this.createColumn()
+    }
+
+    this.setState({
+      isInputDisplayed: false,
+      columnName: '',
+    })
+  }
+
+  onPress = () => {
+    if (!this.state.isInputDisplayed) {
+      this.setState({ isInputDisplayed: true })
+
+      return
+    }
+
+    if (this.columnName) {
+      this.createColumn()
+      this.setState({ columnName: '' })
+    }
+  }
+
+  render() {
+    const { list } = this.props
+    return (
+      <View style={[gStyle.fr1, style.listContainer]}>
+        {
+          list.columns.map((column) => {
+            if (column.isFirstColumn) {
+              return (
+                <View key={column.id} style={gStyle.autoWidth}>
+                  <FirstColumn
+                    listId={list.id}
+                    cells={generateFirstColumn(column, list.rows)}
+                    createRow={this.props.createRow}
+                  />
+                </View>
+              )
+            }
+
+            return null
+          })
+        }
+        <ScrollView style={[gStyle.fr, gStyle.fg0]} horizontal>
+          {
+            list.columns.map((column) => {
+              if (column.isFirstColumn) {
+                return null
+              }
+
+              return (
+                <Column
+                  key={column.id}
+                  listId={list.id}
+                  cells={generateColumn(column, list.rows, list.cells)}
+                />
+              )
+            })
+          }
+        </ScrollView>
+        {
+          this.state.isInputDisplayed &&
+            <View style={columnStyle.textInputWrapper}>
+              <TextInput
+                style={columnStyle.textInput}
+                placeholder="Column name..."
+                autoFocus={true}
+                onChangeText={this.onChangeText}
+                onBlur={this.onBlur}
+                value={this.state.columnName}
+              />
+            </View>
+        }
+        <AddButton onPress={this.onPress} />
+      </View>
+    )
+  }
 }
 
-export default List
+const mapDispatchToProps = { createRow, createColumn }
+
+export default connect(undefined, mapDispatchToProps)(List)
--- src/components/List/List.style.js
+++ src/components/List/List.style.js
@@ -2,13 +2,9 @@ import { StyleSheet } from 'react-native'
 import colors from 'styles/colors'
 
 export default StyleSheet.create({
-  textInput: {
-    height: 50,
-    width: '15%',
-  },
-  list: {
+  listContainer: {
     borderTopWidth: 1,
     borderColor: colors.cellBorder,
-    marginBottom: 30,
+    marginBottom: 30, //TODO is this needed?
   },
 })
